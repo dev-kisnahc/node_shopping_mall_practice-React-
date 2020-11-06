dc638d03bdacd7fe821050b02186016ff024ba04
kisnahc <kisnahc@naver.com>
2020-11-06 15:59:31 +0900

    LoginPage(1)

--- client/src/index.js
+++ client/src/index.js
@@ -8,21 +8,22 @@ import 'antd/dist/antd.css'
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise'
 import ReduxThunk from 'redux-thunk'
-import Reducer from './_reducers/index'
+import Reducer from './_reducers'
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 
 ReactDOM.render(
-  <Provider>
+  <Provider
       store={createStoreWithMiddleware(Reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+         window.__REDUX_DEVTOOLS_EXTENSION__&&
+         window.__REDUX_DEVTOOLS_EXTENSION__()
         )}
+  >
+      <App />
+  </Provider>
 
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
